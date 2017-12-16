c44f518c7e3a8eddf1dfddb37bf3bedfe79c39b4
Cristian Mendoza <thattechnokid@gmail.com>
2017-12-16 12:26:49 -0800

    Got mobile nav and noScroll working, added new MQs for tablet

--- js/mobileNav.js
+++ js/mobileNav.js
@@ -1,15 +1,36 @@
-$("#hamburger").on("click", function (event){
-  $(".mob-div-nav").slideToggle(500);
+// $("#hamburger").on("click", function (){
+//   $(".mob-div-nav").slideToggle(500);
+//
+//     function noscroll() {
+//     window.scrollTo( 0, 0 );
+//   }
+//       // add listener to disable scroll
+//       if ($(".mob-div-nav").css("display") == "block"){
+//         window.addEventListener('scroll', noscroll);
+//         $("#hamburger").css("filter":"invert(100)");
+//       }else if ($(".mob-div-nav").css("display") == "none") {
+//         window.removeEventListener('scroll', noscroll);
+//         $("#hamburger").css("filter":"invert(0)");
+//       }
+// });
 
-    function noscroll() {
-    window.scrollTo( 0, 0 );
-  }
-      // add listener to disable scroll
-      if ($(".mob-div-nav").css("display") == "block"){
-        window.addEventListener('scroll', noscroll);
-
-      }else if ($(".mob-div-nav").css("display") == "none") {
-        window.removeEventListener('scroll', noscroll);
-
-      }
+ // ============  NEW CODE  ===========
+ $(document).ready(function(){
+  // #hamburger click event
+  $("#hamburger").on("click", function (event){
+    $(".mob-div-nav").slideToggle(500);
+  }); // End of #hamburger click event
+  // window scroll event
+  $(window).on('scroll' , noscroll);
 });
+
+// functions here
+// noscroll function
+function noscroll() {
+  if ($(".mob-div-nav").is(':visible')){
+    window.scrollTo( 0, 0 );
+    $("#hamburger").addClass('.invertColors');
+  } else{
+    $("#hamburger").removeClass('.invertColors');
+  }
+}
