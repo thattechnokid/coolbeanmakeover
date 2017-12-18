94b353d75ed0bd98e2d75d6cea9e2905df07d87f
Cristian Mendoza <thattechnokid@gmail.com>
2017-12-18 14:19:23 -0800

    Made mobile changes

--- js/mobileNav.js
+++ js/mobileNav.js
@@ -18,7 +18,7 @@
  $(document).ready(function(){
   // #hamburger click event
   $("#hamburger").on("click", function (event){
-    $(".mob-div-nav").slideToggle(500);
+    $(".mob-div-nav").slideToggle(1000);
   }); // End of #hamburger click event
   // window scroll event
   $(window).on('scroll' , noscroll);
@@ -29,8 +29,5 @@
 function noscroll() {
   if ($(".mob-div-nav").is(':visible')){
     window.scrollTo( 0, 0 );
-    $("#hamburger").addClass('.invertColors');
-  } else{
-    $("#hamburger").removeClass('.invertColors');
   }
 }
